0810bd09339d682875f28d297c9813d0be2d417b
Bassam Ismail <skippednote@gmail.com>
2018-11-07 17:06:17 +0530

    chore: change http to https

--- src/pages/ArticlesPage/index.js
+++ src/pages/ArticlesPage/index.js
@@ -5,7 +5,7 @@ import { Loading } from '../../components/Loading';
 
 const ArticlesResource = createResource(() => {
   return fetch(
-    'http://dev-reactclub.pantheonsite.io/jsonapi/node/article'
+    'https://dev-reactclub.pantheonsite.io/jsonapi/node/article'
   ).then(res => res.json());
 });
 
